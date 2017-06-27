22a97095c4da15c4eac1c3a9778ee52593a12265
aburteaga <aburteaga@gmail.com>
2017-06-27 15:15:21 -0400

    Created .sln, added Entity via NuGet, initialized Class Library

---
+++ RoyalLibraryOfAlexandria/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RoyalLibraryOfAlexandria
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ RoyalLibraryOfAlexandria/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoyalLibraryOfAlexandria.MvcApplication" Language="C#" %>
