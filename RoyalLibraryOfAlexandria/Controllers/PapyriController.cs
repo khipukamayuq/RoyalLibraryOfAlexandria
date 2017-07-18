a537ba3c909b4376cb6c35142b7cec0a30b5142c
aburteaga <aburteaga@gmail.com>
2017-07-17 20:06:29 -0400

    Added some more views, fleshing out controllers papyrus

---
+++ RoyalLibraryOfAlexandria/Controllers/PapyriController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoyalLibraryOfAlexandria.Models;
+
+
+namespace RoyalLibraryOfAlexandria.Controllers
+{
+    public class PapyriController : Controller
+    {
+        private PapyrusRepository papyrusRepository;
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ActuallyCreate(Papyrus papyrus)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Enroll");
+            }
+
+            papyrusRepository = new papyrusRepository();
+            papyrusRepository.Create(papyrus);
+            return RedirectToAction("Create");
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ RoyalLibraryOfAlexandria/Models/Papyrus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RoyalLibraryOfAlexandria.Models
+{
+    public class Papyrus
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+    }
+}
